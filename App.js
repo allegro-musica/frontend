3f242cc5308d55b0f3221ec5208c85d623bd1b56
Waterball <waterballs12@gmail.com>
2021-01-17 17:24:57 +0000

    Add routes

--- App.js
+++ App.js
@@ -1,47 +1,18 @@
 import React from 'react';
-import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={navigation.openDrawer}
-        title="Open navigation drawer"
-      />
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={navigation.openDrawer}
-        title="Open navigation drawer"
-      />
-      <Button
-        onPress={() => navigation.goBack()}
-        title="Go back home"
-      />
-    </View>
-  );
-}
+import Home from "./views/Home";
+import Quizzer from "./views/Quizzer";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home">
+                <Drawer.Screen name="Home" component={Home} />
+                <Drawer.Screen name="Quizzer" component={Quizzer} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
 }
